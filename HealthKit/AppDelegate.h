e20e215fc601df87d4aad2832502e5a07ecba0c9
chuzhaozhi <chuzhaozhi@126.com>
2016-06-02 10:48:31 +0800

    Initial Commit

---
+++ HealthKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthKit
+//
+//  Created by chuzhaozhi on 16/6/2.
+//  Copyright © 2016年 chuzhaozhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
